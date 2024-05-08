e33af40609dd813113ce1dc1b2cdc2cd804b2606
k0tran <>
2024-05-08 14:00:17 +0300

    test.py test structure update

--- test.py
+++ test.py
@@ -2,35 +2,26 @@ import os
 import subprocess
 import sys
 
-def run_co_test(first, second, n1, n2, expected):
+def run_test(cchex, args):
+    cli_args = ['./ccedit_test', '-x', cchex, '-oh'] + args
     proc = subprocess.run(
-        ['./ccedit_test', '-x', first, '-oh', 'crossover', '-x', second, '-1', str(n1), '-2', str(n2)],
+        cli_args,
         capture_output=True,
         text=True,
         check=False
     )
+    return proc.stdout.strip()
 
-    return proc.stdout.strip() == expected
-
-def run_rm_test(cchex, n, expected):
-    proc = subprocess.run(
-        ['./ccedit_test', '-x', cchex, '-oh', 'rm', '-n', str(n)],
-        capture_output=True,
-        text=True,
-        check=False
-    )
-
-    return proc.stdout.strip() == expected
-
-def run_ins_test(cchex, n, c, expected):
-    proc = subprocess.run(
-        ['./ccedit_test', '-x', cchex, '-oh', 'ins', '-n', str(n), '-c', str(c)],
-        capture_output=True,
-        text=True,
-        check=False
-    )
-
-    return proc.stdout.strip() == expected
+def run_tests(test_group_name, test):
+    print('Testing', test_group_name)
+    for i, t in enumerate(test):
+        args = TEST_ARG_MAPPERS[test_group_name](*t[1:-1])
+        if run_test(t[0], args) != t[-1]:
+            print('Error on test: ', t)
+            sys.exit(1)
+        else:
+            print(f'Test #{i} completed')
+    print('')
 
 def test_class(hppfile, cppfile, class_name, clen, tests):
     # Render ccedit.j2
@@ -41,35 +32,23 @@ def test_class(hppfile, cppfile, class_name, clen, tests):
     print('Compiling')
     os.system(f'clang++ ccedit_test.cpp {cppfile} -g -std=c++17 -Iinclude -fsanitize=address,leak -o ccedit_test')
 
-    # Run crossover tests
-    print('Testing crossover')
-    for i, t in enumerate(tests['crossover']):
-        if not run_co_test(*t):
-            print('Error on test: ', t)
-            sys.exit(1)
-        else:
-            print(f'Test #{i} completed')
-    print('')
+    for test_group_name in tests.keys():
+        run_tests(test_group_name, tests[test_group_name])
 
-    # Run rm tests
-    print('Testing rm')
-    for i, t in enumerate(tests['rm']):
-        if not run_rm_test(*t):
-            print('Error on test: ', t)
-            sys.exit(1)
-        else:
-            print(f'Test #{i} completed')
-    print('')
-    
-    # Run ins tests
-    print('Testing ins')
-    for i, t in enumerate(tests['ins']):
-        if not run_ins_test(*t):
-            print('Error on test: ', t)
-            sys.exit(1)
-        else:
-            print(f'Test #{i} completed')
-    print('')
+def co_test_map(second_cc_hex, n1, n2):
+    return ['crossover', '-x', second_cc_hex, '-1', str(n1), '-2', str(n2)]
+
+def rm_test_map(n):
+    return ['rm', '-n', str(n)]
+
+def ins_test_map(n, c):
+    return ['ins', '-n', str(n), '-c', str(c)]
+
+TEST_ARG_MAPPERS = {
+    'crossover': co_test_map,
+    'rm': rm_test_map,
+    'ins': ins_test_map,
+}
 
 TIME_TESTS = {
     'crossover': [
